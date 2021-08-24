c0044e58bf1d11a17d70e62df60cacc779834bf1
Jefferson Bruno <jeffbruno123@gmail.com>
2021-08-24 11:27:05 -0300

    User entity e resource

---
+++ src/main/java/com/jefferson/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.jefferson.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jefferson.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@", "5565656", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
