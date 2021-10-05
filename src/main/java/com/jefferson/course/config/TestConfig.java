5de547847c1c5d89ee4858500bbe2f1b28bc4021
jeffersonbruno111 <jeffbruno123@gmail.com>
2021-10-05 18:27:38 -0300

    OrderStatus Enum

--- src/main/java/com/jefferson/course/config/TestConfig.java
+++ src/main/java/com/jefferson/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.jefferson.course.entities.Order;
 import com.jefferson.course.entities.User;
+import com.jefferson.course.entities.enums.OrderStatus;
 import com.jefferson.course.repositories.OrderRepository;
 import com.jefferson.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "name1", "email1", "num 1", "senha 1");
 		User u2 = new User(null, "name2", "email2", "num 2", "senha 2");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITIING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITIING_PAYMENT, u1);
 		
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
